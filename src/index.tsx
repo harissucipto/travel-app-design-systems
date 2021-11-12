83ae4c6e43c3228e81b26ed198d2a5f0d92921c0
haris sucipto <harisssucipto@gmail.com>
2021-11-12 22:13:56 +0700

    add: Typography Component

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export * from "./@interfaces";
 export * from "./data";
 export * from "./data/images";
 export { ColorBox } from "./ColorBox";
+export { Typography, TypographyProps } from "./Typography";
