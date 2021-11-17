96c4f0e32a762d28fd342430779a695578512d2a
haris sucipto <harisssucipto@gmail.com>
2021-11-17 21:34:08 +0700

    fix: Change inline style to tailwind class component

--- src/ButtonGroup.tsx
+++ src/ButtonGroup.tsx
@@ -23,15 +23,12 @@ export const ButtonGroup = <T extends unknown>({
         return (
           <button
             key={option.value as string}
-            className={classNames(
-              "inline-flex whitespace-nowrap items-center h-10 px-4 font-medium text-sm focus:outline-none hover:bg-gray-50 dark:hover:bg-gray-700 text-gray-700 dark:text-white border border-gray-300 dark:border-gray-500 ",
-              {
-                "rounded-l-lg border-r-0": index === 0,
-                "rounded-r-lg": index === options.length - 1,
-                "bg-gray-50 dark:bg-gray-700": activeOption === option.value,
-                "border-r-0": index !== 0 && index !== options.length - 1,
-              },
-            )}
+            className={classNames("btn-group", {
+              "rounded-l-lg border-r-0": index === 0,
+              "rounded-r-lg": index === options.length - 1,
+              "bg-gray-50 dark:bg-gray-700": activeOption === option.value,
+              "border-r-0": index !== 0 && index !== options.length - 1,
+            })}
             onClick={() => setActiveOption(option.value)}
           >
             {option.content}
