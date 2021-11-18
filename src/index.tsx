f8a4d7d1edecd583be4c298ed2fcc231387a4863
haris sucipto <harisssucipto@gmail.com>
2021-11-18 14:48:44 +0700

    add: Navigation Stories

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export * from "./@interfaces";
 export * from "./data";
 export * from "./data/images";
+
 export * from "./ColorBox";
 export * from "./Typography";
 export * from "./ButtonGroup";
@@ -9,3 +10,6 @@ export * from "./Badge";
 export * from "./Button";
 export * from "./Select";
 export * from "./TextInput";
+
+export * from "./nav/SideNav";
+export * from "./nav/MobileNavbar";
